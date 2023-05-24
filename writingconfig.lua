980183cca7e47cc76c56468f0f13ee532fdceee8
James <logisheen@protonmail.com>
2023-05-24 14:52:45 +0100

    Adding shortcut for Goyo

--- writingconfig.lua
+++ writingconfig.lua
@@ -52,6 +52,7 @@ vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>gy', ':Goyo<CR>', {})
 
 -- Lualine configuration
 require('lualine').setup {
